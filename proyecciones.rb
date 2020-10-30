85a133ea8005b3578b3917691a24b3038230df34
Jorge <jorgesifontes.cl@gmail.com>
2020-10-30 19:54:07 -0300

    comentarios proyecciones.rb

--- proyecciones.rb
+++ proyecciones.rb
@@ -8,13 +8,16 @@ end
 
 data = open("./ventas_base.db").read.split(', ')
 
-res_sim = []
+res_sim = []    #Resultados de simulaciones
 
 2.times do |i|
     #Calculo de argumentos
     #aumento      = (((i + 1) * 0.1) + 1) si i=0 -> 1.1 - si i=1 -> 1.2
     #inicio array = (0 + (6 * i))         si i=0 -> 0   - si i=1 -> 6
     #final array  = (5 + (6 * i))         si i=0 -> 5   - si i=1 -> 11
+    #                              simulacion(datos, aumento, ini, fin)
+    #Simulacion 1: cuando  i=0  -> simulacion( data,     1.1,   0,   5)
+    #Simulacion 2: cuando  i=1  -> simulacion( data,     1.2,   6,  11)
 
     res_sim[i] = sprintf "%0.2f", simulacion(data, ((i + 1) * 0.1) + 1, (0 + (6 * i)), (5 + (6 * i)))
 end
